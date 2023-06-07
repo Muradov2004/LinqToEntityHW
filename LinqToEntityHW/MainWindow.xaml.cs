2392c0a19c0946a347655b28be9b84242b5053ac
Muradov2004 <ami65143@gmail.com>
2023-06-07 13:35:02 +0400

    Add project files.

---
+++ LinqToEntityHW/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace LinqToEntityHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (ComboBox1.SelectedItem == "Authors")
+            {
+
+            }
+        }
+    }
+}
