3d57de55220f7981f052de27393ca78fde37e155
Muradov2004 <ami65143@gmail.com>
2023-06-09 10:05:51 +0400

    Updated combobox2

--- LinqToEntityHW/MainWindow.xaml.cs
+++ LinqToEntityHW/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,6 +10,8 @@ namespace LinqToEntityHW
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Book> books;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,30 +23,87 @@ namespace LinqToEntityHW
             {
                 ComboBoxItem? selectedItem = ComboBox1.SelectedItem as ComboBoxItem;
 
+                books.Clear();
+
                 if (selectedItem!.Content.ToString() == "Authors")
                 {
                     ComboBox2.Items.Clear();
 
                     var authors = database.Authors;
-                    
+
                     authors.ToList().ForEach(a => ComboBox2.Items.Add($"{a.FirstName} {a.LastName}"));
                 }
                 else if (selectedItem!.Content.ToString() == "Themes")
                 {
                     ComboBox2.Items.Clear();
-                    
+
                     var themes = database.Themes;
-                    
+
                     themes.ToList().ForEach(t => ComboBox2.Items.Add($"{t.Name}"));
                 }
                 else if (selectedItem!.Content.ToString() == "Categories")
                 {
                     ComboBox2.Items.Clear();
-                    
+
                     var categories = database.Categories;
-                    
+
                     categories.ToList().ForEach(c => ComboBox2.Items.Add($"{c.Name}"));
                 }
+            }
+        }
+
+        private void ComboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (LibraryContext database = new())
+            {
+                ComboBoxItem? selectedItem = ComboBox1.SelectedItem as ComboBoxItem;
+
+
+                if (selectedItem!.Content.ToString() == "Authors")
+                {
+                    books.Clear();
+
+                    var choosenBooks = database.Books
+                        .Join(
+                        database.Authors,
+                        b => b.IdAuthor,
+                        a => a.Id,
+                        (b,a) =>
+                        new Book()
+                        {
+                            Id = b.Id,
+                            Name = b.Name,
+                            Pages = b.Pages,
+                            YearPress = b.Pages,
+                            IdThemes = b.IdThemes,
+                            IdCategory = b.IdCategory,
+                            IdAuthor = b.IdAuthor,
+                            IdPress = b.IdPress,
+                            Comment = b.Comment,
+                            Quantity = b.Quantity
+                        })
+                         .ToList();
+
+                    choosenBooks.ForEach(b => books.Add(b));
+
+                }
+                else if (selectedItem!.Content.ToString() == "Themes")
+                {
+                    ComboBox2.Items.Clear();
+
+                    var themes = database.Themes;
+
+                    themes.ToList().ForEach(t => ComboBox2.Items.Add($"{t.Name}"));
+                }
+                else if (selectedItem!.Content.ToString() == "Categories")
+                {
+                    ComboBox2.Items.Clear();
+
+                    var categories = database.Categories;
+
+                    categories.ToList().ForEach(c => ComboBox2.Items.Add($"{c.Name}"));
+                }
+
             }
         }
     }
